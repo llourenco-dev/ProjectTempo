cd4607eb16d84709a0595667c9f29003eb387ce9
llourenco-dev <leonardolourenco885@gmail.com>
2026-01-24 17:20:33 +0000

    Login and register UI

--- resources/js/views/Login.vue
+++ resources/js/views/Login.vue
@@ -1,7 +1,61 @@
 <template>
-  <h1>Login Page</h1>
-  <input type="email" placeholder="Email" /><br>
-  <input type="password" placeholder="Password" /><br>
-  <button>Login</button><br>
-  <button>Don't have an account? Register</button>
-</template>
+  <div class="content">
+    <div class="card">
+      <div class="column left-login">
+        <img
+          src="/storage/app/public/images/register_img.png"
+          alt="Register Image"
+        />
+      </div>
+        
+      <div class="column right-login">
+        <h1 class="title">Tempo</h1>
+
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        
+        <router-link
+          to="/register"
+          class="link" style="align-self: flex-end; font-size: 14px;"
+        >
+          Forgot your password?
+        </router-link>
+
+        <button class="btn-primary">Login</button>
+        <div style="display: flex; justify-content: center;">
+          <div class="or-line">
+            <span></span>
+            <span class="or">or</span>
+            <span></span>
+          </div>
+        </div>
+        <div class="social-buttons">
+          <div class="social-btn">
+            <img 
+              src="/storage/app/public/images/google_icon.png"
+              alt="Google Icon"
+            />
+          </div>
+          <div class="social-btn">
+            <img 
+              src="/storage/app/public/images/facebook_icon.png"
+              alt="Google Icon"
+            />
+          </div>
+          <div class="social-btn">
+            <img style="width: 23px;"
+              src="/storage/app/public/images/apple_icon.png"
+              alt="Google Icon"
+            />
+          </div>
+        </div>
+        <router-link
+          to="/register"
+          class="link"
+        >
+          Don't have an account? Register here!
+        </router-link>
+      </div>
+    </div>
+  </div>
+</template> 
--- resources/js/views/Register.vue
+++ resources/js/views/Register.vue
@@ -1,8 +1,54 @@
 <template>
-  <h1>Register Page</h1>
-  <input type="text" placeholder="Name" /><br>
-  <input type="email" placeholder="Email" /><br>
-  <input type="password" placeholder="Password" /><br>
-  <button>Register</button><br>
-  <button>Have an account? Login</button>
-</template>
+  <div class="content">
+    <div class="card">
+      <div class="column left-register">
+        <h1 class="title">Tempo</h1>
+
+        <input type="text" placeholder="Name" />
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+
+        <button class="btn-primary">Register</button>
+        <div style="display: flex; justify-content: center;">
+          <div class="or-line">
+            <span></span>
+            <span class="or">or</span>
+            <span></span>
+          </div>
+        </div>
+        <div class="social-buttons">
+          <div class="social-btn">
+            <img 
+              src="/storage/app/public/images/google_icon.png"
+              alt="Google Icon"
+            />
+          </div>
+          <div class="social-btn">
+            <img 
+              src="/storage/app/public/images/facebook_icon.png"
+              alt="Google Icon"
+            />
+          </div>
+          <div class="social-btn">
+            <img style="width: 23px;"
+              src="/storage/app/public/images/apple_icon.png"
+              alt="Google Icon"
+            />
+          </div>
+        </div>
+        <router-link
+          to="/login"
+          class="link"
+        >
+          Have an account? Login
+        </router-link>
+      </div>
+      <div class="column right-register">
+        <img
+          src="/storage/app/public/images/register_img.png"
+          alt="Register Image"
+        />
+      </div>
+    </div>
+  </div>
+</template> 
