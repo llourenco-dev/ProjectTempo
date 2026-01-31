c40a4e2fd4b23e0b08480c20a6ab5294dc119313
llourenco-dev <leonardolourenco885@gmail.com>
2026-01-31 12:05:59 +0000

    Dashboard improvements, modal for activity details.

--- resources/js/views/Dashboard.vue
+++ resources/js/views/Dashboard.vue
@@ -6,6 +6,8 @@
         <h3>
             {{ day }}
         </h3>
+        
+        <!-- This is the current task the user have, details, etc... -->
         <div class="current-task">
             <div class="task-header">
                 <img src="/public/storage/images/clock_icon.png" alt="Clock">
@@ -33,6 +35,8 @@
                 </p>
             </div>
         </div>
+        
+        <!-- Tasks that the user will have as the day goes on -->
         <div class="today-content">
             <div class="task-header">
                 <img src="/public/storage/images/calendar_icon2.png" alt="Calendar">
@@ -41,12 +45,17 @@
                 </h2>
             </div>
             <div class="task-content" v-if="true">
-                <div class="task-item">
+                <div class="task-item" @click="openTask({
+                    title: 'Task 1 Title',
+                    category: 'Category Name',
+                    start: '10:00 AM',
+                    end: '11:00 AM'
+                })">
                     <div class="content-time">
-                        <p class="time" style="padding-top: 4px;">
+                        <p class="start-time" style="padding-top: 4px;">
                             10:00
                         </p>
-                        <p class="time">
+                        <p class="start-time">
                             AM
                         </p>
                     </div>
@@ -58,21 +67,73 @@
                             Category Name
                         </p>
                     </div>
-                    <div>
+                    <div class="end-time">
                         <p>
-                            11:00 PM
+                            11:00 AM
+                        </p>
+                    </div>
+                </div>
+                <div class="task-item" @click="openTask({
+                    title: 'Task 2 Title',
+                    category: 'Category Name',
+                    start: '12:00 PM',
+                    end: '1:00 PM'
+                })">
+                    <div class="content-time">
+                        <p class="start-time" style="padding-top: 4px;">
+                            12:00
+                        </p>
+                        <p class="start-time">
+                            PM
+                        </p>
+                    </div>
+                    <div>
+                        <p class="content-title">
+                            Task 2 Title
+                        </p>
+                        <p class="content-category">
+                            Category Name
+                        </p>
+                    </div>
+                    <div class="end-time">
+                        <p>
+                            1:00 PM
                         </p>
                     </div>
                 </div>
             </div>
-            <div class="task-content" v-else>
-                
+            <!-- TO DO: Adicionar um else if, caso houve tarefas no dia, aparecer algo de "tarefas diárias acabaram, podes descansar" -->
+            <!--        Caso não houvessem tarefas de todo dar trigger do else statement. -->
+            <div class="task-content" v-else style="text-align: center; margin-bottom: 30px;">
+                <p style="font-size: 30px; color: #778873; margin-bottom: 20px;">
+                    Your day looks empty.
+                </p>
+                <p style="font-size: 18px; color: #778873;">
+                    Set up your tasks to stay productive!
+                </p>
+            </div>
+        </div>
+
+        <div v-if="showModal" class="modal-backdrop" @click.self="closeModal">
+            <div class="modal" @click="closeModal">
+                <div class="modal-header">
+                    <p>close</p>
+                    <h2 class="modal-title">{{ selectedTask.title }}</h2>
+                    <p>Activity Details</p>
+                    <p class="modal-category">{{ selectedTask.category }}</p>
+                </div>
+
+                <p class="modal-time">
+                    {{ selectedTask.start }} → {{ selectedTask.end }}
+                </p>
             </div>
         </div>
     </div>
 </template>
 
 <script setup>
+    import { ref } from 'vue';
+
     const today = new Date()
     const day = today.toLocaleDateString('en-US', {
         month: 'long',
@@ -82,4 +143,17 @@
     const dayOfWeek = today.toLocaleDateString('en-US', {
         weekday: 'long'
     })
+
+    const showModal = ref(false);
+    const selectedTask = ref(null);
+
+    function openTask(task) {
+        selectedTask.value = task;
+        showModal.value = true;
+    }
+
+    function closeModal(){
+        showModal.value = false;
+        selectedTask.value = null
+    }
 </script>
